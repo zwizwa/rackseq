4a49d86dfa00f3888ae56616f8832aa064623ce7
Tom Schouten <tom@zwizwa.be>
2014-07-18 21:36:39 -0400

    yeah maybe racket is not the right platform...

--- test.rkt
+++ test.rkt
@@ -1,2 +1,36 @@
-#lang racket/base
-(printf "rackseq OK\n")
+#lang racket/gui
+
+(define (make-notes-list)
+  (reverse
+   (apply append
+          (for/list ((octave '(2 3 4)))
+            (for/list ((note '("C" "D" "E" "F" "G" "A" "B")))
+              (format "~a-~a" note octave))))))
+    
+
+(define frame (new frame% [label "rackseq"]))
+(define hp (new horizontal-panel%
+                [parent frame]
+                [alignment '(center center)]))
+(for ((i (in-range 16)))
+  (let ((vp (new vertical-panel%
+                 [parent hp]
+                 [alignment '(center center)])))
+    
+    (let ((l (format "b~s" i)))
+      (new choice%
+           [parent vp]
+           [label ""]
+           [choices (make-notes-list)]
+           [callback
+            (lambda (button event) (printf "~s\n" l))])
+      (new check-box%
+           [parent vp]
+           [label ""]
+           [callback
+            (lambda (button event) (printf "~s\n" l))])
+      )))
+
+  (send frame show #t)
+
+
